dce45430a731e920644cfd3b6cbe16f70e5dcc99
YourAverageCoder69 <pfm251@gmail.com>
2020-05-21 16:23:09 +0500

    shit

---
+++ main.py
@@ -0,0 +1,46 @@
+import selenium
+from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+
+
+def get_user(name):
+    while True:
+        try:
+            user = browser.find_element_by_xpath("//span[@title='{}']".format(name))
+            return user
+        except:
+            continue
+
+
+def get_message_box():
+    global browser
+    while True:
+        try:
+            box = browser.find_element_by_xpath(".//*[@id='main']/footer/div[1]/div[2]/div/div[2]")
+            return box
+        except:
+            continue
+
+def start_whatsapp():
+    global browser
+    browser = webdriver.Firefox()
+    browser.get("https://web.whatsapp.com/")
+    global messages
+    messages = [line for line in open("messages.txt", "r")]
+
+
+browser = None
+messages = None
+
+start_whatsapp()
+count = 10
+
+for i in range(count):
+    text = messages[i % len(messages)]
+    text += " Chuttiya"
+    el = get_user('Umar Amjad')
+    el.click()
+    get_message_box().send_keys("YAIN HURT")
+    get_message_box().send_keys(Keys.ENTER)
+
+browser.close()
