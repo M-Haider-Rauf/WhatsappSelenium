1dc831d12b1f7c2108f8b2df7afea8b04cb56ac2
YourAverageCoder69 <pfm251@gmail.com>
2020-05-22 13:57:08 +0500

    added comments

--- main.py
+++ main.py
@@ -3,7 +3,9 @@ import random
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 
-def get_user(name):
+
+# get the contact html element with name 'name'
+def get_contact(name):
     while True:
         try:
             user = browser.find_element_by_xpath("//span[@title='{}']".format(name))
@@ -12,6 +14,7 @@ def get_user(name):
             continue
 
 
+# get the message input box
 def get_message_box():
     global browser
     while True:
@@ -21,26 +24,29 @@ def get_message_box():
         except:
             continue
 
-def start_whatsapp():
+
+# init the driver and load all messages from file
+def init_script():
     global browser
     browser = webdriver.Firefox()
-    browser.get("https://web.whatsapp.com/")
+    browser.get("https://web.whatsapp.com/")  # whatsapp web url
     global messages
+    # load each line from file in list, strip newline characters
     messages = [line.replace('\n', '') for line in open("messages.txt", "r")]
 
 
 browser = None
 messages = None
 
-start_whatsapp()
-spam_count = 20
+init_script()
+spam_count = 20  # number of messages to send
 
 ran_gen = random.Random()
 
 for i in range(spam_count):
-    text = messages[ran_gen.randint(0, len(messages) - 1)]
-    get_user('mama').click()
-    get_message_box().send_keys(text)
-    get_message_box().send_keys(Keys.ENTER)
+    text = messages[ran_gen.randint(0, len(messages) - 1)]  # choose a random message
+    get_contact('mama').click()  # go to contact
+    get_message_box().send_keys(text)  # copy message
+    get_message_box().send_keys(Keys.ENTER)  # press ENTER... done!
 
 browser.close()
